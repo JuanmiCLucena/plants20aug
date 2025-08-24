c97028c2f2a7942596847c02ad19ad0c793653e2
Juanmi <jmcostalucena2000@gmail.com>
2025-08-24 19:12:42 +0200

    Cambiado método de SpeciesService para buscar especies mediante un filtrado

--- force-app/main/default/lwc/speciesList/speciesList.js
+++ force-app/main/default/lwc/speciesList/speciesList.js
@@ -1,8 +1,22 @@
 import { LightningElement, wire } from 'lwc';
-import getAllSpecies from '@salesforce/apex/SpeciesService.getAllSpecies';
+import getFilteredSpecies from '@salesforce/apex/SpeciesService.getFilteredSpecies';
 
 export default class SpeciesList extends LightningElement {
+
+    // Properties, GETTERS & SETTERS
+    searchText = "";
     
-    @wire(getAllSpecies)
+    // LIFECYCLE HOOK
+
+    // WIRE
+    @wire(getFilteredSpecies, { searchText : '$searchText' })
     species;
+
+    // METHODS
+    handleInputChange(event) {
+        const searchTextAux = event.target.value;
+        if(searchTextAux.length >= 3 || searchTextAux === '') {
+            this.searchText = searchTextAux;
+        }
+    }
 }
